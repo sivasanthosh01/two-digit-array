3eb8a318461e82d906f4e0875213f54f0300b6a3
santhosh <killersanthosh0032@gmail.com>
2023-07-19 17:07:54 +0530

    two-digit

---
+++ js/main.js
@@ -0,0 +1,13 @@
+var num=[2,10,8,14,6,20,1,5,100,99];
+document.write("array values:&nbsp;"+num+"<br>");
+document.write("two digit values are:<br>");
+for(i=0;i<num.length;i++)
+	{
+	if((num[i]>9)&&(num[i]<100))
+	{
+		document.write(num[i]+"<br>");
+	}
+	}
+		
+
+	
